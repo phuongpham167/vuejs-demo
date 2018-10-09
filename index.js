2c4c18caadf773044fdfc84d051148b8ac9a6d8b
phuongpham167 <phuongptt.uet@gmail.com>
2018-10-09 10:34:44 +0700

    directive v-on

--- index.js
+++ index.js
@@ -11,3 +11,33 @@ var app2 = new Vue({
   	message: "You loaded this page on" + new Date().toLocaleString()
   }
 })
+
+var app3 = new Vue({
+	el: "#app-3",
+	data: {
+		seen: true
+	}
+})
+
+var app4 = new Vue({
+	el: "#app-4",
+	data:{
+		todos: [
+          { text: "Pham Thi Thu Phuong" },
+          { text: "Nguyen Thanh Cong" },
+          { text: "Vu Hoang Long" }
+		]
+	}
+})
+
+var app5 = new Vue({
+	el: "#app-5",
+    data: {
+      message: "qua lai khach cho song lang song"
+    },
+    methods: {
+      reverseMessage: function(){
+        this.message = this.message.split(" ").reverse().join(" ")
+      }
+    }
+})
